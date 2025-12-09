61b74d1bef292b3463390817337b7c2768eb96ba
bayu jaya <ariesofyan55@gmail.com>
2025-12-09 11:09:02 +0700

    update

---
+++ public/build/assets/settings-DHdC2L44.js
@@ -0,0 +1,21 @@
+import{c as a}from"./createLucideIcon-BF1sQy6q.js";/**
+ * @license lucide-react v0.553.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["path",{d:"M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8",key:"1357e3"}],["path",{d:"M3 3v5h5",key:"1xhq8a"}],["path",{d:"M12 7v5l4 2",key:"1fdv2h"}]],n=a("history",o);/**
+ * @license lucide-react v0.553.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["path",{d:"M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8",key:"5wwlr5"}],["path",{d:"M3 10a2 2 0 0 1 .709-1.528l7-6a2 2 0 0 1 2.582 0l7 6A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z",key:"r6nss1"}]],h=a("house",e);/**
+ * @license lucide-react v0.553.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=[["path",{d:"M13.832 16.568a1 1 0 0 0 1.213-.303l.355-.465A2 2 0 0 1 17 15h3a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2A18 18 0 0 1 2 4a2 2 0 0 1 2-2h3a2 2 0 0 1 2 2v3a2 2 0 0 1-.8 1.6l-.468.351a1 1 0 0 0-.292 1.233 14 14 0 0 0 6.392 6.384",key:"9njp5v"}]],d=a("phone",t);/**
+ * @license lucide-react v0.553.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const s=[["path",{d:"M9.671 4.136a2.34 2.34 0 0 1 4.659 0 2.34 2.34 0 0 0 3.319 1.915 2.34 2.34 0 0 1 2.33 4.033 2.34 2.34 0 0 0 0 3.831 2.34 2.34 0 0 1-2.33 4.033 2.34 2.34 0 0 0-3.319 1.915 2.34 2.34 0 0 1-4.659 0 2.34 2.34 0 0 0-3.32-1.915 2.34 2.34 0 0 1-2.33-4.033 2.34 2.34 0 0 0 0-3.831A2.34 2.34 0 0 1 6.35 6.051a2.34 2.34 0 0 0 3.319-1.915",key:"1i5ecw"}],["circle",{cx:"12",cy:"12",r:"3",key:"1v7zrd"}]],i=a("settings",s);export{h as H,d as P,i as S,n as a};
--- resources/js/Pages/Auth/VerifyEmail.jsx
+++ resources/js/Pages/Auth/VerifyEmail.jsx
@@ -1,8 +1,9 @@
 import PrimaryButton from "@/Components/PrimaryButton";
 import GuestLayout from "@/Layouts/GuestLayout";
-import { Head, Link, useForm } from "@inertiajs/react";
+import { Head, Link, useForm, usePage } from "@inertiajs/react";
 
 export default function VerifyEmail({ status }) {
+    const user = usePage().props.auth.user;
     const { post, processing } = useForm({});
 
     const submit = (e) => {
@@ -16,23 +17,21 @@ export default function VerifyEmail({ status }) {
             <Head title="Email Verification" />
 
             <div className="mb-4 text-sm text-gray-600">
-                Terima Kasih telah mendaftar! Sebelum memulai, dapatkah Anda
-                memverifikasi alamat email Anda dengan mengklik tautan yang baru
-                kami kirimkan melalui email? Jika Anda tidak menerima email,
-                kami dengan senang hati akan mengirimkan email lain kepada Anda.
+                Terima Kasih telah mendaftar! Sebelum memulai, Lakukan
+                verifikasi dengan menekan tombol kirim Link. dan Lakukan
+                verifikasi pada email kamu
             </div>
 
             {status === "verification-link-sent" && (
                 <div className="mb-4 text-sm font-medium text-green-600">
-                    Link baru untuk verifikasi email telah dikirim ke alamat
-                    email yang Anda berikan saat pendaftaran.
+                    Link Telah Di kirimkan ke {user.email}
                 </div>
             )}
 
             <form onSubmit={submit}>
                 <div className="mt-4 flex items-center justify-between">
                     <PrimaryButton disabled={processing}>
-                        Kirim Ulang Email Verifikasi
+                        Kirim Link Verifikasi
                     </PrimaryButton>
 
                     <Link
